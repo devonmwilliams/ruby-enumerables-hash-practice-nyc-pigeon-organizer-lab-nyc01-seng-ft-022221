49a0b15a0eb3026f62ba4d6897fb67a92f4fe044
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-20 23:09:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,24 @@
 binding 'pry'
 
+data = {
+:color => {
+  :purple => ["Theo", "Peter Jr.", "Lucky"],
+  :grey => ["Theo", "Peter Jr.", "Ms. K"],
+  :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+  :brown => ["Queenie", "Alex"]
+},
+:gender => {
+  :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+  :female => ["Queenie", "Ms. K"]
+},
+:lives => {
+  "Subway" => ["Theo", "Queenie"],
+  "Central Park" => ["Alex", "Ms. K", "Lucky"],
+  "Library" => ["Peter Jr."],
+  "City Hall" => ["Andrew"]
+}
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-    data = {
-    :color => {
-      :purple => ["Theo", "Peter Jr.", "Lucky"],
-      :grey => ["Theo", "Peter Jr.", "Ms. K"],
-      :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-      :brown => ["Queenie", "Alex"]
-    },
-    :gender => {
-      :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-      :female => ["Queenie", "Ms. K"]
-    },
-    :lives => {
-      "Subway" => ["Theo", "Queenie"],
-      "Central Park" => ["Alex", "Ms. K", "Lucky"],
-      "Library" => ["Peter Jr."],
-      "City Hall" => ["Andrew"]
-    }
-  }
 hash.each_with_object ({}) do |(key, value), final_array|
   binding.pry
