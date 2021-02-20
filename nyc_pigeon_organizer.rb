ec2a879a34894052067fafdc9754ae3a49ff7b90
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-20 23:32:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pigeon_data.each_with_object({}) do |(key, value), final_array|
+  data.each_with_object({}) do |(key, value), final_array|
   binding.pry
   final_array
   end
