1a8290f8fa905ada9c360de12802af73e3d45c42
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-20 23:22:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,6 @@
 require 'pry'
 
   # write your code here!
-  pigeon_data = {
-    :color => {
-      :purple => ["Theo", "Peter Jr.", "Lucky"],
-      :grey => ["Theo", "Peter Jr.", "Ms. K"],
-      :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-      :brown => ["Queenie", "Alex"]
-    },
-    :gender => {
-      :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-      :female => ["Queenie", "Ms. K"]
-    },
-    :lives => {
-      "Subway" => ["Theo", "Queenie"],
-      "Central Park" => ["Alex", "Ms. K", "Lucky"],
-      "Library" => ["Peter Jr."],
-      "City Hall" => ["Andrew"]
-    }
-  }
 def nyc_pigeon_organizer(pigeon_data)
 pigeon_data.each_with_object({}) do |(key, value), final_array|
   binding.pry
