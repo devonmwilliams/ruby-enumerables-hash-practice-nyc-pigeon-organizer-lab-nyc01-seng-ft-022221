bc2da4e6f7b9c06c464c6c82bcc7bf60c529da25
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-20 23:46:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+  result = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
@@ -9,9 +9,11 @@ def nyc_pigeon_organizer(data)
         end
         if !final_array[name][key]
           final_array[name][key] = []
+        end
+        final_array[name][key] >> inner_key
   binding.pry
 end
-  final_array
+  result
   end
   binding.pry
 end
